75e7cb8827eeffba566449de7ad53b311a3160c4
dpmanohar <deshmukhpradip1202@gmail.com>
2025-12-23 23:23:16 +0530

    Add project files.

---
+++ ProjectForGitAndJenkins/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForGitAndJenkins
+{
+    internal class Program
+    {
+        public void display()
+        {
+            int a = 6;
+            int b = 6;
+            Console.WriteLine("addition is : "+a + b);
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.display();
+        }
+    }
+}
